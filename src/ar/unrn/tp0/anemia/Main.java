1d42d80062aa91461262af06cc04baade07e7c9a
Silver-hub21 <tomsacosta19@gmail.com>
2025-03-11 11:47:17 -0300

    Commit inicial. Declaración del proyecto.

---
+++ src/ar/unrn/tp0/anemia/Main.java
@@ -0,0 +1,7 @@
+package ar.unrn.tp0.anemia;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
---
+++ src/ar/unrn/tp0/anemia/TiempoAnemico.java
@@ -0,0 +1,5 @@
+package ar.unrn.tp0.anemia;
+
+public class TiempoAnemico {
+
+}
